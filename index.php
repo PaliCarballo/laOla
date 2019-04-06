19510d7e987b23a51ced059ec754052b7205e2a4
danysaavedra <danielesaavedra2015@gmail.com>
2019-04-06 15:28:43 -0300

    cambios nav y foot

--- index.php
+++ index.php
@@ -5,48 +5,74 @@
     <title>La Ola</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
+    <link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
+
   </head>
   <body>
     <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg navbar-light bg-success">
-        <img src="img/logo.png" id="logo" alt="">
-
+      <nav class="navbar navbar-expand-lg navbar-dark ">
+        <img src="img/logo.png" class="logo" alt="">
+&nbsp;<h1>LA OLA / HAMBURGUESAS VEGGIES</h1>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home&nbsp;<ion-icon name="home"></ion-icon><span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+        <a class="nav-link" href="#">Preguntas Frecuentes&nbsp;<ion-icon name="help"></ion-icon></a>
+              </li>
+              <li class="nav-item active">
+        <a class="nav-link" href="#">Registrate&nbsp;<ion-icon name="information-circle"></ion-icon></a>
+              </li>
+              <li class="nav-item active">
+        <a class="nav-link" href="#">Login&nbsp;<ion-icon name="log-in"></ion-icon></a>
+              </li>
+              <li class="nav-item active">
+        <a class="nav-link" href="#">Hace tu Pedido&nbsp;<ion-icon name="cart"></ion-icon></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Preguntas Frecuentes</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Registrate</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-
     </ul>
-
   </div>
 </nav>
-    </div>
+</div>
+
+
+
+<br><br><br>
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+<footer>
+    <h3>Seguinos en nuestras redes sociales</h3><br>
+  <ul>
+          <li>
+              <a href="#"> <ion-icon name="logo-facebook"></ion-icon>facebook</a>
+          </li>
+          <li>
+              <a href="#"><ion-icon name="logo-instagram"></ion-icon>Instagram</a>
+          </li>
+          <li>
+              <a href="#"> <ion-icon name="logo-twitter"></ion-icon>Twitter</a>
+          </li>
+          <li>
+              <a href="#"> <ion-icon name="logo-whatsapp"></ion-icon>whatsapp</a>
+          </li>
+  </ul>
+    <h4>Copyright-2019-la Ola S.A</h4>
+</footer>
+</div>
 
 
 
@@ -59,8 +85,9 @@
 
 
 
+    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
 </html>
