173b92eb8ab5627b8ade1c4f30998606fcad89ac
Mercemendezpaz <mercedesmendezpaz@gmail.com>
2019-04-08 13:04:33 -0300

    aca van los php de log y registro, y un css para los dos. Saludos, merce

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/logyregistro.css">
+    <title>La Ola - login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/logyregistro.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+    <form action="index.php" method="get">
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="mail">
+  </div>
+  <div class="form-group">
+    <label for="pass">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+  </div>
+
+  <button type="submit" class="btn btn-outline-light">Enviar :)</button>
+</form>
+
+
+
+
+
+
+
+  </body>
+</html>
---
+++ registro.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La Ola - registro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="css/logyregistro.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+  </head>
+  <body>
+
+
+    <form action="" method="post">
+      <ul>
+
+
+      <li><label for="">Nombre</label>
+      <input type="text" name="dire" value=""></li>
+
+      <li><label for="">Apellido</label>
+      <input type="text" name="apellido" value=""></li>
+
+      <li><label for="">Email</label>
+      <input type="email" name="" value=""></li>
+
+      <li><label for="">Contraseña</label>
+      <input type="password" name="pass" value=""></li>
+
+      <li><label for="">Dirección</label>
+      <input type="text" name="dire" value="">
+      <label for="">piso, depto., etc.</label>
+      <input type="text" name="dire2" value="">
+      <label for="">Codigo Postal</label>
+      <input type="number" name="codigopostal" value="" >
+      </li>
+
+      <li><label for="inputState">Ciudad</label>
+      <select id="inputState" class="form-control" name="ciudad">
+        <option selected>Seleccione...</option>
+        <option>CABA</option>
+        <option>Gran Buenos Aires</option>
+        <option>Ushuaia</option>
+      </select></li>
+
+      <li><label for="">Provincia</label>
+      <select class="" name="provincia">
+        <option selected>Seleccione...</option>
+        <option value="">Buenos Aires</option>
+        <option value="">Tierra del Fuego</option>
+      </select></li>
+      <li><button type="submit" class="btn btn-outline-light">Registrate!</button></li>
+
+      </ul>
+
+    </form>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
