b37237435e4420569ef36ad221da8ad6c6786a80
PaliCarballo <pal.carballo11@gmail.com>
2019-04-09 13:23:22 -0300

    Optimizacion estilos de nav y footer

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('nav.php'); ?>
+
+<div class="fondo-banner">
+  <h1>HAMBURGUESAS</h1>
+  <h2>VEGANAS</h2>
+  <p>100% Artesanales | Sin Conservantes</p>
+</div>
+
+
+
+
+
+
+
+<?php include('footer.php'); ?>
--- nav.php
+++ nav.php
@@ -8,14 +8,19 @@
     <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/index.css">
     <link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
 
   </head>
   <body>
     <div class="container-fluid">
       <nav class="navbar navbar-expand-lg navbar-dark ">
+
+  
         <img src="img/logo.png" class="logo" alt="">
-&nbsp;<h1>LA OLA / HAMBURGUESAS VEGGIES</h1>
+        &nbsp;<h1>LA OLA / HAMBURGUESAS VEGGIES</h1>
+
+
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,40 +28,21 @@
 
     <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-        <a class="nav-link" href="index.php"><ion-icon name="home"></ion-icon>&nbsp;Home</a>
+        <a class="nav-link" href="index.php"><ion-icon name="home"></ion-icon><br>Home</a>
               </li>
               <li class="nav-item active">
         <a class="nav-link" href="#"><ion-icon name="help"></ion-icon><br>Preguntas Frecuentes</a>
               </li>
               <li class="nav-item active">
-        <a class="nav-link" href="#"><ion-icon name="information-circle"></ion-icon>&nbsp;Registrate</a>
+        <a class="nav-link" href="#"><ion-icon name="information-circle"></ion-icon><br>Registrate</a>
               </li>
               <li class="nav-item active">
-        <a class="nav-link" href="#"><ion-icon name="log-in"></ion-icon>&nbsp;Login</a>
+        <a class="nav-link" href="#"><ion-icon name="log-in"></ion-icon><br>Login</a>
               </li>
               <li class="nav-item active">
-        <a class="nav-link" href="#"><ion-icon name="cart"></ion-icon><br>&nbsp;Hace tu Pedido</a>
+        <a class="nav-link" href="#"><ion-icon name="cart"></ion-icon><br>Hace tu Pedido</a>
       </li>
     </ul>
   </div>
 </nav>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
-</html>
