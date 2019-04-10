fbc8d22604a685ad242c32d4354420290de5da95
PaliCarballo <pal.carballo11@gmail.com>
2019-04-10 17:08:46 -0300

    Mas modificaciones en index.php

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 <div class="fondo-banner">
   <div class="texto-banner">
       <h1>HAMBURGUESAS</h1>
-      <h2>VEGANAS</h2>
+      <h2>VEGGIES</h2>
       <p>100% Artesanales & Sin Conservantes</p>
       
-      <div class="btn btn-primary">Conocé más de nosotros</div>
-      <div class="btn btn-primary">Hacé tu pedido</div>
+      
   </div>
+  <div class="btn btn-primary">Conocé más de nosotros</div>
+      <div class="btn btn-primary">Hacé tu pedido</div>
+
 </div>
 
 
