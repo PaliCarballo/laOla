ea931f87036e8a4c4c2cf3526a0eca944bb5652e
PaliCarballo <pal.carballo11@gmail.com>
2019-04-15 13:52:49 -0300

    Marge index.css

--- index.php
+++ index.php
@@ -6,12 +6,64 @@
       <h2>VEGGIES</h2>
       <p>100% Artesanales & Sin Conservantes</p>
   </div>
-  <div class="btn btn-primary">Conocé más de nosotros</div>
-  <div class="btn btn-primary">Hacé tu pedido</div>
-
+  <div class="btn btn-primary"><a href="#sobreNosotros">Conocé más de nosotros</a></div>
+  <div class="btn btn-primary"><a href="carrito.php">Hacé tu pedido</a></div>
 </div>
 
+<section id="productosDestacados">
+  <div class="titulo-productos">
+    <h3>Productos Destacados</h3>
+    <hr>
+  </div>
+      <div class="productos">
+          <article class="producto">
+              <img src="img/combo1.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
+          <article class="producto">
+              <img src="img/combo2.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
+          <article class="producto">
+              <img src="img/combo3.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
+          <article class="producto">
+              <img src="img/combo2.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
+          <article class="producto">
+              <img src="img/combo3.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
+          <article class="producto">
+              <img src="img/combo1.jpg" alt="" class="cel-img">
+          <!--    <img src="img/img-descuento.png" alt="" class="cel-flag">
+              <p class="cel-name">COMBO 1</p>
+              <a href="carrito.php" class="cel-more">Ver más</a> -->
+          </article>
 
+      </div>
+</section>
+
+<section id="sobreNosotros">
+  <h3>- TE LLEVA LA OLA -</h3>
+  <p>Creemos que aportar valor es fundamental para nuestro crecimiento. Queremos acompañarte en mejorar tu alimentación y así puedas sentir vitalidad y energía en cada día. Realizamos nuestra labor con dedicación y amor, donde cuidar cada detalle es importante. <br>
+  Por ello ofrecemos un producto artesanal, donde los sabores se conservan tal cual a la materia prima, <strong> 100% natural, sin agregados artificiales ni químicos.</strong> 
+  La Ola te propone preparar tus comidas de una manera simple y sana.
+<img src="img/icono-logo.png" alt=""></p>
+
+</section>
 
 
 
