231397ca8b52c56ee77650edf87bc72e47179bb3
PaliCarballo <pal.carballo11@gmail.com>
2019-04-15 21:17:47 -0300

    Merge index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       <p>100% Artesanales & Sin Conservantes</p>
   </div>
   <div class="btn btn-primary">Conocé más de nosotros</div>
-  <div class="btn btn-primary">Hacé tu pedido</div>
+  <div class="btn btn-primary"><a href="carrito.php">Hacé tu pedido</a></div>
 
 </div>
 
